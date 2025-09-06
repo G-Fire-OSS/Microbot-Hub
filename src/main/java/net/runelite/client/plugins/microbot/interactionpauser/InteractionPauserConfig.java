3eec66cc14bc36e2a992f66034f39e0a2892abcb
FireRat <27763388+FireRat666@users.noreply.github.com>
2025-09-07 09:51:10 +1000

    stuff

--- src/main/java/net/runelite/client/plugins/microbot/interactionpauser/InteractionPauserConfig.java
+++ src/main/java/net/runelite/client/plugins/microbot/interactionpauser/InteractionPauserConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("interactionPauser")
+@ConfigGroup("micro-interactionpauser")
 public interface InteractionPauserConfig extends Config {
     @ConfigItem(
             keyName = "pauseDuration",
